dbc22146c99ead1daccc6e8a97890878460f0f9c
ejc2140 <ejc2140@g.rit.edu>
2022-02-26 17:12:45 -0500

    title

--- wichacks/src/App.js
+++ wichacks/src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
       <header className="App-header">
+        <h1>Raccoon Rex’s Library of Internet Safety</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
