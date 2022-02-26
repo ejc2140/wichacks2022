00742cbe407b62ae211f3188154063bd793a1980
ejc2140 <ejc2140@g.rit.edu>
2022-02-26 15:57:29 -0500

    new files

--- wichacks/src/index.js
+++ wichacks/src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Parent from './parent';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Router>
+
+       <Routes>
+
+        <Route path='/' element={<App/>} />
+        <Route path="/Parent" element={<Parent />}></Route>
+
+        </Routes>
+  </Router>,
   document.getElementById('root')
 );
 
---
+++ wichacks/src/parent.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Sidebar from './Sidebar';
+
+function Parent() {
+  return (
+    <div className="App">
+      <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default Parent;
