b4a70b6aa2810ced07d1898b059d18876a97cc89
ejc2140 <ejc2140@g.rit.edu>
2022-02-27 10:20:09 -0500

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- wichacks/src/App.js
+++ wichacks/src/App.js
@@ -9,7 +9,9 @@ function App() {
       <header className="App-header">
         <title>Raccoon Rex’s Library of Internet Safety</title>
         <h1>Raccoon Rex’s Library of Internet Safety</h1>
+        <p>Click on the side menu to get started! </p>
         <img src="rex_quote.png" />
+        <p>Join Raccoon Rex on his journey to helping his friends be internet safe!</p>
       </header>
     </div>
   );
