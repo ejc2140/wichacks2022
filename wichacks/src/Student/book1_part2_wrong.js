b41e49917ae04d5d0eba48d3f3ffee02b4fe6390
jaimecamp <jc5755@rit.edu>
2022-02-27 10:41:22 -0500

    added book 1 part 2 right and wrong pages

---
+++ wichacks/src/Student/book1_part2_wrong.js
@@ -0,0 +1,26 @@
+import logo from '../logo.svg';
+import '../App.css';
+import Sidebar from '../Sidebar';
+
+function Book1_Part2_Wrong() {
+  return (
+    <div className="App">
+      <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+      <header className="App-header">
+        <h1>Raccoon Rex’s Library of Internet Safety</h1>
+        <p>Book 1: The Monsterific Playdate</p>
+        <img src="/Book1/book1_part2_right.jpg" width="700" height="400"/>
+        <p className='black_text'>
+          Question 2 
+          <br></br>
+          What should Clyde do?</p>
+        <a href="/book2"> <button type="button">Option 1</button></a>
+        <a href="/book2"> <button type="button">Option 2</button></a>
+        <br></br>
+        
+      </header>
+    </div>
+  );
+}
+
+export default Book1_Part2_Wrong;
