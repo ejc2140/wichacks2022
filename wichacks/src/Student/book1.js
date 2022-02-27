0f11ae023e8fb1aa58c3c8beb3f8005ebb5e1219
jaimecamp <jc5755@rit.edu>
2022-02-27 10:10:24 -0500

    added the book1 image

--- wichacks/src/Student/book1.js
+++ wichacks/src/Student/book1.js
@@ -8,9 +8,8 @@ function Book1() {
       <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
       <header className="App-header">
         <h1>Raccoon Rex’s Library of Internet Safety</h1>
-        <p>
-            Book 1!
-         </p>
+        <p>Book 1: The Monsterific Playdate</p>
+        <img src="/Book1/book1_part1.jpg" width="700" height="400"/>
         
       </header>
     </div>
