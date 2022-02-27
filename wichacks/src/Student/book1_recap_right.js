130cfda009fbc6ae0a53f611a5de9601ac96d7fe
jaimecamp <jc5755@rit.edu>
2022-02-27 10:57:54 -0500

    made recap pages

---
+++ wichacks/src/Student/book1_recap_right.js
@@ -0,0 +1,19 @@
+import logo from '../logo.svg';
+import '../App.css';
+import Sidebar from '../Sidebar';
+
+function Book1_Recap_Right() {
+  return (
+    <div className="App">
+      <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+      <header className="App-header">
+        <h1>Raccoon Rex’s Library of Internet Safety</h1>
+        <p>Book 1: The Monsterific Playdate</p>
+        You made both the right choices!
+        
+      </header>
+    </div>
+  );
+}
+
+export default Book1_Recap_Right;
